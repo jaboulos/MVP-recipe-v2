e8f3b2bb15db1fa60c70dcf7bf8060e487d03635
Joe Boulos <jab12288@gmail.com>
2019-03-23 11:42:16 -0700

    styling recipe component

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -15,7 +15,20 @@ const Recipes = ({recipeList}) => (
           <tr key={item.id}>
             <td>
               <div>
-                <h2><strong>{item.email}</strong></h2>
+                <h2><strong>RECIPE SUBMITTED BY:<h5>{item.email}</h5> </strong></h2>
+              </div>
+              <div>
+                <h5><strong>NAME: </strong></h5>{item.recipesName}
+              </div>
+              <div>
+                <h5><strong>COOK TIME: </strong></h5>{item.recipeTime}
+              </div>
+              <div>
+                <h5><strong>INGREDIENTS:</strong></h5> {item.recipeIngredients}
+              </div>
+              <div>
+                <h5><strong>INSTRUCTIONS: </strong></h5>
+                {item.recipeInstructions}
               </div>
             </td>
           </tr>

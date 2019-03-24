087913a8236a0465cae0b205d6357b82947149e3
Joe Boulos <jab12288@gmail.com>
2019-03-24 16:28:24 -0700

    building out profile component for the artists

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,8 +4,37 @@ import '../App.css';
 
 class Profile extends Component {
   render() {
+    // console.log('this.props ', this.props)
+    let artist = {
+      name: '',
+      followers: {total: ''},
+      images: [{url: ''}],
+      genres: []
+    }
+    if(this.props.artist !== null) {
+      artist = this.props.artist
+    }
     return(
-<div>something</div>
+      <div className="Profile">
+        <img
+          alt="Profile"
+          className="profile-img"
+          src={artist.images[0].url}
+        />
+        <div className="profile-info">
+          <div className="profile-name">Artist: {artist.name}</div>
+          <div className="profile-followers">Followers: {artist.followers.total}</div>
+          <div className="profile-genres"> Genres: { artist.genres.map((genre, k) => {
+            // commas between genres
+            genre = genre !== artist.genres[artist.genres.length-1] ? ` ${genre},` : ` & ${genre}`
+              return (
+              <span key={k}>{genre}</span>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
     )
   }
 }

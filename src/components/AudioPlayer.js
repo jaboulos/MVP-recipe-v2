cc5bb7fa2f968371ef3b1f16009eb37986586c07
Joe Boulos <jab12288@gmail.com>
2019-03-24 14:52:38 -0700

    successful api call to spotify

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
-// import { FormGroup, FormControl, InputGroup, Glyphicon, Button } from 'react-bootstrap';
+import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
 
 
 // spotify endpoint, dont need api key
@@ -52,7 +52,7 @@ class AudioPlayer extends Component {
 */
 
 search() {
-  console.log('this.state', this.state);
+  // console.log('this.state', this.state);
   const BASE_URL = 'https://api.spotify.com/v1/search?';
   const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
   var accessToken = 'BQCXXDjU9wbR4i7MmZrjCYAr04zQAv6KH1L5wOeQO5BUKbNWuGqilZZLkZA5ufTESocA-UKjs8y50puWGZSKgs31O-JGmOxesLgn1ljx20FNuhZQfsxQE3pWTO4HWSHmhYjlRlnX3jUixjoVpOsDuvRxlUKOIbNI8x96gG4Z&refresh_token=AQBR0PIzmZFGPrmR6Cl_lDBfTlCVIzdmMplygur8VHxDk3Vv-eqWPR_58Sl2WbgVcaFgCXJHi7_s6hv8Gcc_jytmYy-lgF3d9ZAPe_-1tI'
@@ -71,52 +71,11 @@ search() {
     .then(json => {
       const artist = json.artists.items[0];
       this.setState({ artist });
+      console.log('this.state', this.state);
     })
-
 }
 
-  // render() {
-  //   return (
-  //     <div className="Audioplayer">
-  //       <div className="Audioplayer-title">Audio Player</div>
-  //      {/* React-Bootstrap components */}
-  //       <FormGroup>
-  //         {/* wrap form control with InputGroup */}
-  //         <InputGroup>
-  //           {/* this is the equivalent of an input field, FormControl is also self-closing */}
-  //           <FormControl
-  //               type="text"
-  //               placeholder="Search for an artist..."
-  //               value={this.state.query}
-  //               onChange={event => {this.setState({query: event.target.value})}}
-  //               onKeyPress={event => {
-  //                 // if the user clicks enter or mouse click, call this.search()
-  //                 // console.log('event.key: ', event.key)
-  //                 if(event.key === 'Enter') {
-  //                   // call this.search
-  //                   this.search()
-  //                 }
-  //               }}
-  //             />
-  //             {/* react-bootstrap has some cool add-ons, this inputgroup.add-on changes the button icon on this form, find more in the react bootstrap documentation library */}
-  //           {/* <InputGroup.Addon>
-  //             <Glyphicon glyph="search"></Glyphicon>
-  //           </InputGroup.Addon> */}
-  //           <Button onClick={()=> this.search()}>PLAY SOME MUSIC!</Button>
-  //         </InputGroup>
-  //       </FormGroup>
-  //       <div className="Profile">
-  //         <div>Artist Picture</div>
-  //         <div>Artist Name</div>
-  //       </div>
-  //       <div className="Gallery">
-  //         Gallery
-  //       </div>
-  //     </div>
-  //   )
-  // }
   render() {
-
     let artist = {
       name: '',
       followers: {
@@ -126,33 +85,83 @@ search() {
     if (this.state.artist !== null) {
       artist = this.state.artist;
     }
-
     return (
-      // return JSX
-      <div className="container">
-        <hr />
-        <div className="col-lg-6">
-          <div className="input-group">
-            <input type="text"
-              onChange={event => { this.setState({ query: event.target.value }) }}
-            className="form-control" placeholder="Search for..." />
-            <span className="input-group-btn">
-              <button
-              onClick={()=> this.search()}
-               className="btn btn-default" type="button">Go!</button>
-            </span>
-          </div>
+      <div className="Audioplayer">
+        <div className="Audioplayer-title">Audio Player</div>
+       {/* React-Bootstrap components */}
+        <FormGroup>
+          {/* wrap form control with InputGroup */}
+          <InputGroup>
+            {/* this is the equivalent of an input field, FormControl is also self-closing */}
+            <FormControl
+                type="text"
+                placeholder="Search for an artist..."
+                value={this.state.query}
+                onChange={event => {this.setState({query: event.target.value})}}
+                onKeyPress={event => {
+                  // if the user clicks enter or mouse click, call this.search()
+                  // console.log('event.key: ', event.key)
+                  if(event.key === 'Enter') {
+                    // call this.search
+                    this.search()
+                  }
+                }}
+              />
+              {/* react-bootstrap has some cool add-ons, this inputgroup.add-on changes the button icon on this form, find more in the react bootstrap documentation library */}
+            {/* <InputGroup.Addon>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon> */}
+            <Button onClick={()=> this.search()}>PLAY SOME MUSIC!</Button>
+          </InputGroup>
+        </FormGroup>
+        <div className="Profile">
+          <div>Artist: {artist.name}</div>
+          <div>Followers: {artist.followers.total}</div>
         </div>
-        <hr />
-        <div>
-          <div> {artist.name}   </div>
-          <div> {artist.followers.total} </div>
+        <div className="Gallery">
+          Gallery
         </div>
-
-
-        </div>
+      </div>
     )
   }
+  // render() {
+
+  //   let artist = {
+  //     name: '',
+  //     followers: {
+  //       total: ''
+  //     }
+  //   };
+  //   if (this.state.artist !== null) {
+  //     artist = this.state.artist;
+  //   }
+
+  //   return (
+  //     // return JSX
+  //     <div className="container">
+  //       <hr />
+  //       <div className="col-lg-6">
+  //         <div className="input-group">
+  //           <input type="text"
+  //             onChange={event => { this.setState({ query: event.target.value }) }}
+  //           className="form-control" placeholder="Search for..." />
+  //           <span className="input-group-btn">
+  //             <button
+  //             onClick={()=> this.search()}
+  //              className="btn btn-default" type="button">Go!</button>
+  //           </span>
+  //         </div>
+  //       </div>
+  //       <hr />
+  //       <div>
+  //         <div> {artist.name}   </div>
+  //         <div> {artist.followers.total} </div>
+  //       </div>
+
+
+  //       </div>
+  //   )
+  // }
 }
 
 export default AudioPlayer;

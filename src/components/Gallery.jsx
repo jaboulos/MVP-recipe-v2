23c0241f2388eecb29cd9d81b4342a873e8abb7a
Joe Boulos <jab12288@gmail.com>
2019-03-25 12:13:58 -0700

    audio now can be paused and resumed when user clicks on play button

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,9 +3,40 @@ import React, { Component } from 'react';
 import './AudioPlayer';
 
 class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      playingUrl: '',
+      audio: null,
+      playing: false
+    }
+  }
+
   playAudio(previewUrl) {
     let audio = new Audio(previewUrl);
-    audio.play()
+    if(!this.state.playing) {
+      audio.play()
+      this.setState({
+        playing: true,
+        playingUrl: previewUrl,
+        audio
+      })
+    } else {
+      if (this.state.playingUrl === previewUrl) {
+        this.state.audio.pause();
+        this.setState({
+          playing: false
+        })
+      } else {
+        this.state.audio.pause();
+        audio.play();
+        this.setState({
+          playing: true,
+          playingUrl: previewUrl,
+          audio
+        })
+      }
+    }
   }
 
   render() {
@@ -16,13 +47,13 @@ class Gallery extends Component {
           tracks.map((track, k) => {
             console.log('track ', track)
             return(
-
-                <div
-                  key={k}
-                  className="track"
-                  onClick={()=>this.playAudio(track)}
-                >
-                PLAY TRACK ONE
+                <div key={k} className="track" onClick={()=>this.playAudio(track)}>
+                  <div>PLAY TRACK</div>
+                  <div className="track-play">
+                    <div className="track-play-inner">
+                      &#9654;
+                    </div>
+                    </div>
                 </div>
 
             )

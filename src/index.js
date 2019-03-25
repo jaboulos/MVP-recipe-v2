d519836b271aceb4514f67ca057d6d1ae8bf7847
Joe Boulos <jab12288@gmail.com>
2019-03-25 13:36:44 -0700

    added signup, signout, signin components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 import Homepage from '../src/components/Homepage';
-// import SignUp from './components/SignUp';
+import SignUp from './components/SignUp';
 import SignIn from '../src/components/SignIn';
 import Dashboard from '../src/components/Dashboard';
 // import Recipes from './components/Recipes';
@@ -17,7 +17,8 @@ ReactDOM.render(
   <BrowserRouter>
     <App>
       <Route exact path="/" component={Homepage}/>
-      {/* <Route exact path="/signup" component={SignUp}/> */}
+      <Route exact path="/signout" component={Homepage}/>
+      <Route exact path="/signup" component={SignUp}/>
       <Route exact path="/signin" component={SignIn}/>
       {/* <Route exact path="/recipes" component={Recipes}/> */}
       <Route exact path="/dashboard" component={Dashboard}/>

41b56b0f6ed549c1772503f80513f7e59a0ac7d8
Joe Boulos <jab12288@gmail.com>
2019-03-24 17:21:24 -0700

    code cleanup

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -20,36 +20,6 @@ class AudioPlayer extends Component {
       artist: null
     }
   }
-/*
-  // test to see if query is working
-  search() {
-    // console.log("THIS IS THE STATE: ", this.state);
-    // declare spotify search endpoint as a base url
-    // question mark at the end of the url allows for more parameters
-    const BASE_URL = 'https://api.spotify.com/v1/search?';
-    // declare fetch url
-    const FETCH_URL = `${BASE_URL}q=this.state.query&type=artist&limit=1`
-    console.log('FETCH_URL: ', FETCH_URL)
-
-    const accessToken = 'BQCXXDjU9wbR4i7MmZrjCYAr04zQAv6KH1L5wOeQO5BUKbNWuGqilZZLkZA5ufTESocA-UKjs8y50puWGZSKgs31O-JGmOxesLgn1ljx20FNuhZQfsxQE3pWTO4HWSHmhYjlRlnX3jUixjoVpOsDuvRxlUKOIbNI8x96gG4Z&refresh_token=AQBR0PIzmZFGPrmR6Cl_lDBfTlCVIzdmMplygur8VHxDk3Vv-eqWPR_58Sl2WbgVcaFgCXJHi7_s6hv8Gcc_jytmYy-lgF3d9ZAPe_-1tI'
-
-    //   var myOptions = {
-    //     method: 'GET',
-    //     headers: {
-    //       'authorization': accessToken
-    //     },
-    //     mode: 'cors',
-    //     cache: 'deafult'
-    //   }
-
-    //   fetch(FETCH_URL, myOptions)
-    //     .then(response => response.json())
-    //     .then(json => {
-    //       const artist = json.artists.items[0]
-    //       this.setState({ artist })
-    //     })
-  }
-*/
 
 search() {
   // console.log('this.state', this.state);
@@ -107,17 +77,9 @@ search() {
                   }
                 }}
               />
-              {/* react-bootstrap has some cool add-ons, this inputgroup.add-on changes the button icon on this form, find more in the react bootstrap documentation library */}
-            {/* <InputGroup.Addon>
-              <Glyphicon glyph="search"></Glyphicon>
-            </InputGroup.Addon> */}
             <Button onClick={()=> this.search()}>PLAY SOME MUSIC!</Button>
           </InputGroup>
         </FormGroup>
-        {/* <div className="Profile">
-          <div>Artist: {artist.name}</div>
-          <div>Followers: {artist.followers.total}</div>
-        </div> */}
         <Profile
           artist={this.state.artist}
         />
@@ -127,44 +89,6 @@ search() {
       </div>
     )
   }
-  // render() {
-
-  //   let artist = {
-  //     name: '',
-  //     followers: {
-  //       total: ''
-  //     }
-  //   };
-  //   if (this.state.artist !== null) {
-  //     artist = this.state.artist;
-  //   }
-
-  //   return (
-  //     // return JSX
-  //     <div className="container">
-  //       <hr />
-  //       <div className="col-lg-6">
-  //         <div className="input-group">
-  //           <input type="text"
-  //             onChange={event => { this.setState({ query: event.target.value }) }}
-  //           className="form-control" placeholder="Search for..." />
-  //           <span className="input-group-btn">
-  //             <button
-  //             onClick={()=> this.search()}
-  //              className="btn btn-default" type="button">Go!</button>
-  //           </span>
-  //         </div>
-  //       </div>
-  //       <hr />
-  //       <div>
-  //         <div> {artist.name}   </div>
-  //         <div> {artist.followers.total} </div>
-  //       </div>
-
-
-  //       </div>
-  //   )
-  // }
 }
 
 export default AudioPlayer;
